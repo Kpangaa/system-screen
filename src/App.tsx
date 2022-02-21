9664f220ea4f38efd3c3e6593c4d777a950d38b4
Kpanga <hernan.moya16@gmail.com>
2022-02-21 15:46:41 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import ModalSuccessFullSystemDamage from "./components/componentViewModal/viewSu
 import ModalTimerSystemDamage from "./components/componentViewModal/viewTimerModalSystemDamage/ModalTimerSystemDamage";
 import { getColorRandom } from "./util/GetColorRandom";
 import { screenTestChallenge } from "./components/net/Connector";
+import Desactivar from "./Desactivar";
 
 function isTouchEvent(e: React.TouchEvent | React.MouseEvent): e is React.TouchEvent {
     return e && 'touches' in e;
@@ -76,6 +77,8 @@ const App = () => {
     const location = useLocation();
     const query = new URLSearchParams(location.search);
     const challengeId = query.get('id');
+    console.log(`location`, location)
+    console.log(`challengeId`, challengeId)
 
     /** Variables Modal */
     const [showStartModalSystem, setShowStartModalSystem] = useState<boolean>(false);
@@ -140,6 +143,7 @@ const App = () => {
                     setShowSuccessfullModalSystem(true);
                 } catch (error) {
                     // genericErrorHandler(error);
+                    console.log(`error`, error)
                     setShowSuccessfullModalSystem(false);
                     setViewStatus(ViewModalSystem.FAILED);
                 }
@@ -208,6 +212,7 @@ const App = () => {
             }
             await axios.put(screenTestChallenge(challengeId), response);
         } catch (error) {
+            console.log(`error 2`, error)
             // genericErrorHandler(error);
         }
     }
@@ -262,7 +267,21 @@ const App = () => {
                         backgroundColor: '#f4f4f4',
                     }
                 }}>
-                <ModalStartSystemDamage
+                    <Desactivar
+                    desactivarPressed={() => {
+                        setShowStartModalSystem(true);
+                        toggleFullScreen();
+                        startTest = true;
+                        window.addEventListener("resize", () => {
+                            setWidth(window.innerWidth);
+                            setHeight(window.innerHeight);
+                            setCantidadX(Math.ceil(window.innerWidth / DIMENSION_SIZE));
+                            setCantidadY(Math.ceil(window.innerHeight / DIMENSION_SIZE));
+                            setContador((Math.ceil(window.innerWidth / DIMENSION_SIZE) * Math.ceil(window.innerHeight / DIMENSION_SIZE)));
+                        });
+                    }}
+                    />
+                {/* <ModalStartSystemDamage
                     onStartTest={() => {
                         setShowStartModalSystem(true);
                         toggleFullScreen();
@@ -275,7 +294,7 @@ const App = () => {
                             setContador((Math.ceil(window.innerWidth / DIMENSION_SIZE) * Math.ceil(window.innerHeight / DIMENSION_SIZE)));
                         });
                     }}
-                />
+                /> */}
             </Modal>
         )
     }
